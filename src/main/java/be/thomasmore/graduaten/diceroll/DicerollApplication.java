9a01ad7fca50d34569559606d0fe27c02d194ba1
Proewkster <proewkster@hotmail.com>
2020-10-19 19:23:53 +0200

    Init DiceRoll Project

---
+++ src/main/java/be/thomasmore/graduaten/diceroll/DicerollApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.graduaten.diceroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicerollApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DicerollApplication.class, args);
+    }
+
+}
